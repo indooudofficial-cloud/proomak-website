ad10235b115dab17fe33d0463797854b4acd9390
indooudofficial-cloud <indooudofficial@gmail.com>
2026-05-21 19:02:28 +0800

    Delete api/test-env (2).js

--- api/test-env (2).js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    ZOHO_USER: process.env.ZOHO_USER || "Not set",
-    ZOHO_PASS_set: process.env.ZOHO_PASS ? true : false
-  });
-}
